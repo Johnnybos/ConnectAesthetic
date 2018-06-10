7ccc7aac22e6b646d98d9302627377d7fe8922d2
Asaad Khattab <asaad1592@gmail.com>
2018-06-10 18:19:13 -0400

    Added EJS

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
+const http = require('http');
 const hapi = require("hapi");
-const server = new hapi.server();
+const server = new hapi.Server();
 
-//Port
 server.connection({port:3000});
 
 server.start(console.log("Hello World"));
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(request, reply){
+    reply.view("Test");
+  }
+})
+
+server.register(require("vision"), function(err){
+  server.views({
+    engine: {
+      ejs: require("ejs")
+    },
+    relativeTo: __dirname,
+    path: "views"
+  })
+});
+
+server.register(require("inert"), function(err){
+
+});
+
+server.route({
+  method: "GET",
+  path: "/{param}",
+  handler: {
+    directory: {
+      path: "public"
+    }
+  }
+});
